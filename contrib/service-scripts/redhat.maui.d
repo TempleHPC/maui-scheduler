0d4b6e14805f6da5d6b54e262cf4f743ed079096
josh <josh@3f5042e3-fb1d-0410-be18-d6ca2573e517>
2007-01-25 21:01:11 +0000

    FEATURE:	added startup script for RedHat Contact:	Dave Chin Description:	Dave Chin contributed this trivial modification to the Maui startup script on-line

---
+++ contrib/service-scripts/redhat.maui.d
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# maui	This script will start and stop the MAUI Scheduler
+#
+# chkconfig: 345 85 85
+# description: maui
+#
+ulimit -n 32768
+# Source the library functions
+. /etc/rc.d/init.d/functions
+
+MAUI_PREFIX=/opt
+
+# let see how we were called
+case "$1" in
+	start) 
+		echo -n "Starting MAUI Scheduler: "
+		daemon --user maui $MAUI_PREFIX/sbin/maui
+		echo
+		;;
+	stop)
+		echo -n "Shutting down MAUI Scheduler: "
+		killproc maui
+		echo
+		;;
+	status)
+		status maui
+		;;
+	restart)
+		$0 stop
+		$0 start
+		;;
+	*)
+		echo "Usage: maui {start|stop|restart|status}"
+		exit 1
+esac
