5a6ae4693b36e5b1d5d000b279c715c065566963
Richard Berger <richard.berger@temple.edu>
2016-06-22 23:04:43 -0400

    Enable automatic HTML docs generation
    Adds a script which lets Travis CI build the HTML documentation using
    Sphinx and then commits it to the gh-pages branch.

---
+++ tools/generateDocsAndDeploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+GH_REPO_REF=github.com/${TRAVIS_REPO_SLUG}.git
+
+###########################################
+# generate HTML documentation using Sphinx
+###########################################
+git clone -b gh-pages https://${GH_REPO_REF} gh-pages
+
+# configure git user to be Travis CI
+git config --global push.default simple
+git config user.name "Travis CI"
+git config user.email "travis@travis-ci.org"
+
+# clear branch contents
+cd gh-pages
+rm -rf *
+touch .nojekyll
+
+# build HTML
+cd ../docs
+make guide
+
+# commit to gh-pages branch
+mv rst/_build/html ../gh-pages/docs
+cd ../gh-pages
+
+# TODO enable this code before merging to master
+#if [ "$TRAVIS_BRANCH" != "master" ]
+#then
+#  echo "This commit was made against $TRAVIS_BRANCH and not the master! Not deploying updated documentation!"
+#  exit 0
+#fi
+
+if [ -d "docs" ] && [ -f "docs/index.html" ]; then
+    git add --all
+    git commit -m "Deploy docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
+    git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+else
+    echo '' >&2
+    echo 'Warning: No documentation (html) files have been found!' >&2
+    echo 'Warning: Not going to push the documentation to GitHub!' >&2
+    exit 1
+fi
