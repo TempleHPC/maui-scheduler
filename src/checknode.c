c068e48a7b337029ab9956cead940f07f379ba51
dalin <tuf69906@temple.edu>
2016-07-18 15:10:44 -0400

    rewrites checknode

--- src/checknode.c
+++ src/checknode.c
@@ -1,5 +1,5 @@
 /*
- * checkjob standalone client program code
+ * checknode standalone client program code
  *
  * (c) 2016 Temple HPC Team
  */
@@ -7,29 +7,23 @@
 #include "msched-version.h"
 #include "maui_utils.h"
 
-#define OFF 1
-#define SOFT 3
-#define HARD 4
 #define VERBOSE 4
 #define AVP 6
 
-/** Struct for checkjob options */
-typedef struct _checkjob_info {
-    int   pType;               /**< Policy type*/
-    char *jobid;               /**< Job ID */
+/** Struct for checknode options */
+typedef struct _checknode_info {
+    char *nodeid;              /**< Node ID */
     int   flags;			   /**< Flags */
-    char *resid;			   /**< Reservation ID */
-    char *nodeid;			   /**< Node ID */
-} checkjob_info_t;
+} checknode_info_t;
 
-static void free_structs(checkjob_info_t *, client_info_t *);
-static int process_args(int, char **, checkjob_info_t *, client_info_t *);
+static void free_structs(checknode_info_t *, client_info_t *);
+static int process_args(int, char **, checknode_info_t *, client_info_t *);
 static void print_usage();
-static char *buildMsgBuffer(checkjob_info_t );
+static char *buildMsgBuffer(checknode_info_t );
 
 int main(int argc, char **argv) {
 
-    checkjob_info_t checkjob_info;
+    checknode_info_t checknode_info;
     client_info_t client_info;
 
     char *response, request[MAXBUFFER], *msgBuffer;
@@ -39,11 +33,11 @@ int main(int argc, char **argv) {
     char configDir[MAXLINE];
     char *host;
 
-    memset(&checkjob_info, 0, sizeof(checkjob_info));
+    memset(&checknode_info, 0, sizeof(checknode_info));
     memset(&client_info, 0, sizeof(client_info));
 
     /* process all the options and arguments */
-    if (process_args(argc, argv, &checkjob_info, &client_info)) {
+    if (process_args(argc, argv, &checknode_info, &client_info)) {
 
 		/* get config file directory and open it*/
 		strcpy(configDir, MBUILD_HOMEDIR);
@@ -80,8 +74,8 @@ int main(int argc, char **argv) {
 		if (!connectToServer(&sd, port, host))
 			exit(EXIT_FAILURE);
 
-		msgBuffer = buildMsgBuffer(checkjob_info);
-		generateBuffer(request, msgBuffer, "checkjob");
+		msgBuffer = buildMsgBuffer(checknode_info);
+		generateBuffer(request, msgBuffer, "checknode");
 		free(msgBuffer);
 
 		if (!sendPacket(sd, request))
@@ -106,39 +100,26 @@ int main(int argc, char **argv) {
 
     }
 
-    free_structs(&checkjob_info, &client_info);
+    free_structs(&checknode_info, &client_info);
 
     exit(0);
 }
 
 /* combine and save information into a buffer */
-char *buildMsgBuffer(checkjob_info_t checkjob_info) {
+char *buildMsgBuffer(checknode_info_t checknode_info) {
 	char *buffer;
 	int len = 0;
 
-	if(checkjob_info.nodeid == NULL)
-		checkjob_info.nodeid = string_dup(NONE);
-
-	if(checkjob_info.resid == NULL)
-		checkjob_info.resid = string_dup(NONE);
-
 	/* calculate the length of the whole buffer */
-	len += strlen(checkjob_info.jobid) + 2;
-	len += strlen(checkjob_info.resid) + 2;
-	len += strlen(checkjob_info.nodeid) + 2;
+	len += strlen(checknode_info.nodeid) + 2;
 
-    if (checkjob_info.pType == 0)
-    	checkjob_info.pType = SOFT;
-
-	if ((buffer = (char *) malloc(len + 5)) == NULL) {
+	if ((buffer = (char *) malloc(len + 4)) == NULL) {
 		puts("ERROR: cannot allocate memory for buffer");
 		return NULL;
 	}
 
 	/* build buffer */
-	sprintf(buffer, "%d %s %d %s %s", checkjob_info.pType,
-			checkjob_info.jobid, checkjob_info.flags, checkjob_info.resid,
-			checkjob_info.nodeid);
+	sprintf(buffer, "%s %d", checknode_info.nodeid, checknode_info.flags);
 
 	return buffer;
 }
@@ -150,7 +131,7 @@ char *buildMsgBuffer(checkjob_info_t checkjob_info) {
 */
 
 int process_args(int argc, char **argv,
-                 checkjob_info_t *checkjob_info,
+                 checknode_info_t *checknode_info,
                  client_info_t *client_info)
 {
     int c;
@@ -195,53 +176,11 @@ int process_args(int argc, char **argv,
               break;
 
           case 'A':
-        	  checkjob_info->flags |= (1 << AVP);
-              break;
-
-          case 'l':
-        	  switch (optarg[0]) {
-				case 'o':
-				case 'O':
-					/* o-OFFPOLICY */
-					checkjob_info->pType = OFF;
-					break;
-
-				case 'h':
-				case 'H':
-					/* h-HARDPOLICY */
-					checkjob_info->pType = HARD;
-					break;
-
-				case 's':
-				case 'S':
-					/* s-SOFTPOLICY */
-					checkjob_info->pType = SOFT;
-					break;
-
-				default:
-		            print_usage();
-		            exit(EXIT_FAILURE);
-					break;
-				}
-			  break;
-
-          case 'n':
-              if (strlen(optarg) >= (MAXLINE << 2)) {
-				fprintf(stderr, "ERROR: expression too long. (%d > %d)\n",
-						(int)strlen(optarg), (MAXLINE << 2));
-
-				exit(EXIT_FAILURE);
-              }
-
-              checkjob_info->nodeid = string_dup(optarg);
-              break;
-
-          case 'r':
-        	  checkjob_info->resid = string_dup(optarg);
+        	  checknode_info->flags |= (1 << AVP);
               break;
 
           case 'v':
-        	  checkjob_info->flags |= (1 << VERBOSE);
+        	  checknode_info->flags |= (1 << VERBOSE);
               break;
 
           case 'C':
@@ -274,7 +213,7 @@ int process_args(int argc, char **argv,
 
           case '?':
               /* getopt_long already printed an error message. */
-              puts ("Try 'checkjob --help' for more information.");
+              puts ("Try 'checknode --help' for more information.");
               return 0;
 
           default:
@@ -289,17 +228,15 @@ int process_args(int argc, char **argv,
         exit(EXIT_FAILURE);
     }
 
-    /* copy and save job id from input */
-    checkjob_info->jobid = string_dup(argv[optind]);
+    /* copy and save node id from input */
+    checknode_info->nodeid = string_dup(argv[optind]);
 
     return 1;
 }
 
 /* free memory */
-void free_structs(checkjob_info_t *checkjob_info, client_info_t *client_info) {
-    free(checkjob_info->jobid);
-    free(checkjob_info->nodeid);
-    free(checkjob_info->resid);
+void free_structs(checknode_info_t *checknode_info, client_info_t *client_info) {
+    free(checknode_info->nodeid);
     free(client_info->configfile);
     free(client_info->host);
     free(client_info->logfacility);
@@ -307,19 +244,14 @@ void free_structs(checkjob_info_t *checkjob_info, client_info_t *client_info) {
 
 void print_usage()
 {
-    puts ("\nUsage: checkjob [FLAGS] <JOBID>\n\n"
-            "Display detailed job state information and diagnostic output for a specified job.\n"
+    puts ("\nUsage: checknode [FLAGS] <NODEID>\n\n"
+            "Show detailed state information and statistics for nodes that run jobs.\n"
             "\n"
             "  -h, --help                     display this help\n"
             "  -V, --version                  display client version\n"
     		"\n"
     		"  -A, --avp                      provide output in the form of parsable Attribute-Value\n"
     		"                                   pairs\n"
-    		"  -l, --policylevel=POLICYLEVEL  report job start eligibility subject to specified\n"
-    		"                                   throttling policy level. POLICYLEVEL can be 'hard',\n"
-    		"                                   'soft', or 'off'\n"
-    		"  -n, --node=NODEID              check job access to specified node\n"
-    		"  -r, --reservation=RESID        check job access to specified reservation\n"
-    		"  -v, --verbose                  display detailed availability information about nodes\n");
+    		"  -v, --verbose                  display information about location\n");
     print_client_usage();
 }
